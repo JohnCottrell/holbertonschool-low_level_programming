f9c5efd5f9b97327240fd1c47adf675c12be88ba
JohnCottrell <155@holbertonschool.com>
2017-05-11 20:41:22 +0000

    C 0x01 Project 0: Correction of comments and spacing, after betty check.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - this function does something
+ * @some_variable: this var doesn't exist
+ * Return: zero under all circumstances
+ **/
+
 int main(void)
 {
 	int n;
@@ -22,9 +26,9 @@ int main(void)
 			printf("%d is zero\n", n);
 		}
 		else
-		{	
+		{
 			printf("%d is negative\n", n);
 		}
-	}	
+	}
 	return (0);
 }

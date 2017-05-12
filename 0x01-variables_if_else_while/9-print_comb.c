ef1c838065aeb6a9d3bfcb3408be612011edca1f
JohnCottrell <155@holbertonschool.com>
2017-05-12 05:07:19 +0000

    C 0x01 Project 9, corrected.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,12 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		if (i <= 9)
+		if (i < '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar('\n');
-		}
 	}
+	putchar('\n');
 	return (0);
 }

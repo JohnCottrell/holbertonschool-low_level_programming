68bd8f7d802ef56873c51899385aa37e3ad0e178
JohnCottrell <155@holbertonschool.com>
2017-05-12 00:10:56 +0000

    C 0x01 Project 3 though the file is 4: first push.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e')
+			continue;
+		if (a == 'q')
+			continue;
+		putchar (a);
+	}
+	{
+		putchar ('\n');
+	}
+	return (0);
+}

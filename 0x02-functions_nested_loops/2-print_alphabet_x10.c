a3d696040775aaa47348950b42ce5adeab6a4070
JohnCottrell <155@holbertonschool.com>
2017-05-16 21:27:36 +0000

    0x02 Nested functions C Project 2, second commit, correction because of endless loop.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,17 @@
  */
 void print_alphabet_x10(void)
 {
-	int j = 0;
+	int i, j;
+
+	j = 0;
 
 	while (j < 10)
 	{
-		int i;
-
 		for (i = 97; i < 123; i++)
+		{
 			_putchar(i);
-
+		}
 		_putchar('\n');
+		j++;
 	}
-	j++;
 }

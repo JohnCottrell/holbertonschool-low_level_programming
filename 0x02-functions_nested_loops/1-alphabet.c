680180e11718bf5d4f63823c2599578d2e90bef7
JohnCottrell <155@holbertonschool.com>
2017-05-16 20:16:37 +0000

    0x03 Project 1, first push 1-alphabet.c and holberton.h.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}

2b6556cd35ee77f0baf044aa2c42420deeb3f1bf
JohnCottrell <155@holbertonschool.com>
2017-05-16 23:38:25 +0000

    0x02 nested functions Project 5 Correction of description of print-sign in comments, change c to n and addition of star for betty checker.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,10 @@
 
 /**
  * print_sign - print the sign of a number
- * @c: Test the n variable
  *
- * Return: 1 print + if n>0, return 0 print 0 if=0, return -1 print - if less 0
+ * @n: Test the n variable for + - or 0
+ *
+ * Return: 1 print + if n >0, return 0 print 0 if=0, return -1 print - if < 0
  */
 int print_sign(int n)
 {

f527cb9cb76ccb0f481c850a57918408f4840466
JohnCottrell <155@holbertonschool.com>
2017-05-17 17:59:32 +0000

    0x02 Nesting functions, Project 9, corrections after peer learning.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,5 +7,36 @@
  */
 void times_table(void)
 {
+	int a, b, c, d, e;
 
+	for (a = 0; a < 10; a++)
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			d = c / 10;
+			e = c % 10;
+			if (b == 0)
+			{
+				_putchar('0');
+			}
+			else if (c < 10)
+			{
+				_putchar(' ');
+				_putchar(e + '0');
+			}
+			else
+			{
+				_putchar(d + '0');
+				_putchar(e + '0');
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
 }

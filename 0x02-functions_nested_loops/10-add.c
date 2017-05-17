f2e1f3479da82992771e2af38a561de3002b0883
JohnCottrell <155@holbertonschool.com>
2017-05-17 19:17:50 +0000

    0x02 Nesting functions Projec 10 a+b, first push.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers and return the results
+ * @a: integer a passed in from main
+ * @b: integer b passed in from main
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}

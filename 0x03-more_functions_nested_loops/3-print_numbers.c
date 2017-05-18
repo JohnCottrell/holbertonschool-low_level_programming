cd03f99b274335fbbf994b7df714dc24ca8f8c11
JohnCottrell <155@holbertonschool.com>
2017-05-18 22:53:47 +0000

    0x03 More functions, Project 3 print zero to nine.

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9, followed by a new line
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+	int x;
+
+	x = 0;
+
+	while (x < 9)
+	{
+		x++;
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}

2afde10aaf01c07d4ef082c8403b29d455ea9516
JohnCottrell <155@holbertonschool.com>
2017-05-18 22:10:01 +0000

    0x03 More functiions... Project 2, collaboration is multiplication, First push.

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ *@a: Multiply a with b
+ *@b: Multiply b with a
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}

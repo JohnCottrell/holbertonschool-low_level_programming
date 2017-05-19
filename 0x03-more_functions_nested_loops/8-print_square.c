94afbf1d89365260f30a7282f1e075c95d2d6cd1
JohnCottrell <155@holbertonschool.com>
2017-05-19 07:08:59 +0000

    Project 8.

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints a square followed by a new line
+ * @size: size of square to be printed
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	x = 0;
+
+	while (x <= size)
+	{
+		y = 0;
+		while (y <= size)
+		{
+			_putchar('#');
+			++y;
+		}
+		_putchar('#');
+		++x;
+	}
+}

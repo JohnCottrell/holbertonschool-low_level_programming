a2b5ddd759925ce4c821c9d8121b454fd0f195c9
JohnCottrell <155@holbertonschool.com>
2017-05-19 21:05:11 +0000

    0x03 More functions Project 9 correction of spacing and new line.

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,27 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; ++a)
+	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if (((a % 3) == 0) && ((a % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((a % 3) == 0)
 		{
 			printf("Fizz");
 		}
-		if ((a % 5) == 0)
+		else if ((a % 5) == 0)
 		{
 			printf("Buzz");
 		}
-		if ((a % 3 != 0) && (a % 5 != 0))
+		else
 		{
-			printf("FizzBuzz");
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");

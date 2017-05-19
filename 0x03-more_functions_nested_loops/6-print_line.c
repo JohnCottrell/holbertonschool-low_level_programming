17943161a62a842d1d93a1c4d9eb4b4645d6c081
JohnCottrell <155@holbertonschool.com>
2017-05-19 03:14:24 +0000

    0x03 More functions, Project 6, print straight lines, First push.

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *@n: number of lines to be printed
+ *Return: void
+ */
+void print_line(int n)
+{
+	int x;
+
+	x = 0;
+
+	while ((n >= x) && (n > 0))
+	{
+		_putchar('_');
+		x++;
+	}
+	_putchar('\n');
+}

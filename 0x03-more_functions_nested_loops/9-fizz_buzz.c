5631eb351ca0ef1cfaffa858d0a61564c848daca
JohnCottrell <155@holbertonschool.com>
2017-05-19 06:58:18 +0000

    0x03 More functions, Project 9, needs work, first push.

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; ++a)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if ((a % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		if ((a % 3 != 0) && (a % 5 != 0))
+		{
+			printf("FizzBuzz");
+		}
+	}
+	printf("\n");
+	return (0);
+}

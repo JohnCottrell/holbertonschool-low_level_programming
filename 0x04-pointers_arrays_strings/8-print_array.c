83966c94623322e0e20fca95589eeae0f284af8b
JohnCottrell <155@holbertonschool.com>
2017-05-23 22:31:14 +0000

    0x04 Pointer, Arrays, Strings, Project 8, first commit.

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of array of integers, followed by new line
+ * @a: array to be processsed
+ * @n: number of elements of the array to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}

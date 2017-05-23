7fbba346901f941779674064122541f7304fcf87
JohnCottrell <155@holbertonschool.com>
2017-05-23 21:14:08 +0000

    0x04 Pointers, Arrays, Strings, Project 7, first commit.

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half of a string, followed by a new line
+ * @str: string to be processed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, i, n;
+
+	len = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		len++;
+	}
+	len = len / 2;
+	for (n = len; str[n]; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}

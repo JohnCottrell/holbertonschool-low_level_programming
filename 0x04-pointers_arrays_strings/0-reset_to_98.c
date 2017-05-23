3afa557519560faa5a78a2249c9d1b2a7ec86b83
JohnCottrell <155@holbertonschool.com>
2017-05-23 16:41:47 +0000

    '*n = 98;' 0x04 pointers arrays strings, project 0 and header file first push.

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - update teh value of the int pointer to the value of 98
+ * @n: pointer to an int as parameter
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

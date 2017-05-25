1928a3d8c21a08404985c13ec210b4defb9b836c
JohnCottrell <155@holbertonschool.com>
2017-05-25 23:36:57 +0000

    0x05 more pointers, Project 3, second push.

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,30 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2, i, n;
+	int i;
 
-	len1 = 0;
-	len2 = 0;
-
-	for (i = 0; s1[i]; i++)
+	i = 0;
+	while (s1[i] == s2[i])
 	{
-		len1++;
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+			i++;
 	}
-	for (i = 0; s2[i]; i++)
-	{
-		len2++;
-	}
-	if (len1 < 0)
-	{
-		n = len2 - len1;
-	}
-	else if (len1 > 0)
-	{
-		n = len1 - len2;
-	}
-	else
-	{
-		n = 0;
-	}
-	return (n);
+	return (s1[i] - s2[i]);
 }

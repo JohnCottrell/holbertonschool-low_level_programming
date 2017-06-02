394e9ffc28d7234bd323bd9aeead98a502d00ec9
JohnCottrell <155@holbertonschool.com>
2017-06-02 22:54:14 +0000

    0x07 Recursion, addition of main test functions.

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - returns 1 if input integer is prime, otherwise returns 0
+ * @n: number to be examined
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+
+}

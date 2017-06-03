d2185e2d286fd975d9c353fc243fc4b137123d5c
JohnCottrell <johnpcottrell@gmail.com>
2017-06-03 05:29:31 +0000

    0x07 Recursion, project 5.

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,1 +1,35 @@
 #include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be examined
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	return (multi(n, 1));
+}
+
+/**
+ * multi - multiplies the second argument
+ * @n: number to be examined
+ * @mul: multiplier
+ * Return: int
+ */
+int multi(int n, int mul)
+{
+	if ((mul * mul) == n)
+	{
+		return (mul);
+	}
+
+	else if ((mul * mul) > n)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (multi(n, (mul + 1)));
+	}
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -8,3 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int diviz(int n, int div);
+int multi(int n, int mul);

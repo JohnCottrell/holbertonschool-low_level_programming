28ca72048752297afa3e6f84f73a16382cea783e
JohnCottrell <johnpcottrell@gmail.com>
2017-06-03 06:58:02 +0000

    0x07 Recursion, project 7,

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,9 +1,30 @@
 #include "holberton.h"
 
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be measured
+ * Return: int
+*/
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 0;
+
+	if (s[0] != '\0')
+	{
+		len++;
+		return (len + _strlen_recursion(++s));
+	}
+	else
+	{
+		return (len);
+	}
+}
+
+/**
  * is_palindrome - returns 1 if a string is a palindrome, or 0 if not
  * @s: string to be examined
- * @start: beginning of string
  * @end: end of string
  * Return: int
  */

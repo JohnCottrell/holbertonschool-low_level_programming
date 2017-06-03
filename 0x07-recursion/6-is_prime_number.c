470e38d44713997ff43e7072c277795b8a08ece2
JohnCottrell <johnpcottrell@gmail.com>
2017-06-03 05:05:31 +0000

    0x07 Recursion, Project 6.

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -7,5 +7,36 @@
  */
 int is_prime_number(int n)
 {
+	if (n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (diviz(n, 2));
+	}
+}
 
+/**
+ * diviz - divides an integer to determine if it is prime
+ * @n: number to be divided
+ * @div: divisor
+ * Return: int
+ */
+int diviz(int n, int div)
+{
+	if (div == n)
+	{
+		return (1);
+	}
+
+	else if (n % div == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (diviz(n, (div + 1)));
+	}
 }

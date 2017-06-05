e77130b73e675c121c71f10d90cf7ad3a4b99961
JohnCottrell <155@holbertonschool.com>
2017-06-05 23:51:19 +0000

    0x09 project 5, 100-change.c.

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints minimum number of coins to make change for a given amount
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: success is 0.
+ */
+int main(int argc, char *argv[])
+{
+
+}

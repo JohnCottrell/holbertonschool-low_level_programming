ff075a56b801d80914e7cd81ea6e9d5feb27749e
JohnCottrell <johnpcottrell@gmail.com>
2017-06-06 04:08:59 +0000

    0x09 Project 5.

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints minimum number of coins to make change for a given amount
@@ -8,5 +9,38 @@
  */
 int main(int argc, char *argv[])
 {
+	int quarter, dime, nickel, tuppence, penny, coins, change;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	change = atoi(argv[1]);
+
+	if (change < 0)
+	{
+		printf("0\n");
+	}
+
+	quarter = 25;
+	dime = 10;
+	nickel = 5;
+	tuppence = 2;
+	penny = 1;
+	coins = 0;
+
+	coins = change / quarter;
+	change = change % quarter;
+	coins += change / dime;
+	change = change % dime;
+	coins += change / nickel;
+	change = change % nickel;
+	coins += change / tuppence;
+	change = change % tuppence;
+	coins += change / penny;
+
+	printf("%d\n", coins);
+
+	return (0);
 }

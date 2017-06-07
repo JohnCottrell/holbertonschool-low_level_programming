95e0abe73d0d93338c15ef1c96e301e0e66d18ec
JohnCottrell <155@holbertonschool.com>
2017-06-07 23:11:10 +0000

    0x0A Project 5.

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: number of command line arguments
+ * @av: passed arguments
+ * Return: new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *newstring;
+
+	int i, j, k, len;
+
+	len = 0;
+	k = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 1; i < ac; i++)
+			for (j = 0; av[i][j]; j++)
+			{
+				len++;
+			}
+		newstring = malloc((len + 1 + ac) * sizeof(char));
+		for (i = 1; i < ac; i++)
+		{
+			for (j = 0; av[i][j]; j++)
+			{
+				newstring[k++] = av[i][j];
+			}
+			newstring[k++] = '\n';
+		}
+		newstring[k] = '\0';
+	}
+	return (newstring);
+}

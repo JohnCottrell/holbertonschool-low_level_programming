ed9272e4a2a165ad204a672235887bd8e703e85e
JohnCottrell <johnpcottrell@gmail.com>
2017-06-09 06:01:17 +0000

    0x0B Advanced project first push.

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _realloc - rellocates a memory block with malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: old size of allocated memory
+ * @new_size: new size of allocated memory
+ * Return: pointer to new memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *newptr;
+
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	newptr = malloc(new_size * sizeof(unsigned int));
+	if (newptr == NULL)
+		return (NULL);
+	for (i = 0; i <= old_size; i++)
+		newptr[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (newptr);
+}

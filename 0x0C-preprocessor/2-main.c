6f8e39fc2426c5ddfb35c8180fd074e06adde6d6
JohnCottrell <johnpcottrell@gmail.com>
2017-06-12 22:05:25 +0000

    0x0C Project 2, addition of desription of main.

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * 2-main.c prints the name of the file it was compiled from
+ * main - prints the name of the file it was compiled from
  * Return: Always 0.
  */
 int main(void)

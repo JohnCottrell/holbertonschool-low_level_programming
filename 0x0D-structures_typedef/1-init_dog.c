6ba189ac26d493fbde58a8369daaa54401d8ec5e
JohnCottrell <johnpcottrell@gmail.com>
2017-06-13 01:11:17 +0000

    0x0D Project 1, first push.

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != 0)
+	{
+		;
+	}
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}

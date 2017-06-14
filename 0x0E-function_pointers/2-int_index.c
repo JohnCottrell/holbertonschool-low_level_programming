7030e25cd294d5a5a6cafa1d5254d55a4c9a2d78
JohnCottrell <johnpcottrell@gmail.com>
2017-06-14 23:29:33 +0000

    0x0E Project 2, first commit.

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to be searched
+ * @size: number of elements in the array
+ * @cmp: pointer to function to be used to compare values
+ * Return: -1 for no matches or size less than or equal to zero, or index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, check;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		check = (*cmp)(array[i]);
+		if (check != 0)
+		{
+			return (i);
+		}
+
+	}
+	return (-1);
+}

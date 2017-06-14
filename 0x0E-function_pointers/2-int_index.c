8e65f559afcd01950a3c90f7132f7cc8cbc8019c
JohnCottrell <johnpcottrell@gmail.com>
2017-06-14 23:48:40 +0000

    0x0E Project 2, add check for array and cmp being null.

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,18 +12,21 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, check;
 
-	if (size <= 0)
+	if (array != NULL && cmp != NULL)
 	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		check = (*cmp)(array[i]);
-		if (check != 0)
+		if (size <= 0)
 		{
-			return (i);
+			return (-1);
 		}
+		for (i = 0; i < size; i++)
+		{
+			check = (*cmp)(array[i]);
+			if (check != 0)
+			{
+				return (i);
+			}
 
+		}
 	}
 	return (-1);
 }

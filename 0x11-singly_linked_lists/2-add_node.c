b702ec48d570d88d6243a797c73f523e418f47d4
JohnCottrell <johnpcottrell@gmail.com>
2017-06-23 00:06:42 +0000

    project 2 first push.

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,12 +1,41 @@
 #include "lists.h"
+#include <string.h>
+
+/**
+ * _strlen - calculates length of string
+ * @str: string to be measured
+ * Return: length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: very first pointer to linked list, hence head
  * @str: address of node to be preceeded by new node
- * Returns: address of new element or NULL for failure
+ * Return: address of new element or NULL for failure
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }

77729d6dc15b998e1c7410c4ef3b1e2ef12f2905
JohnCottrell <155@holbertonschool.com>
2017-06-27 22:21:30 +0000

    remove head pointer test for null.

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,10 +13,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	unsigned int count;
 
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
@@ -36,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{	new->n = n;
-		new->next  = h->next;
+		new->next = h->next;
 		h->next = new;
 	}
 	return (new);

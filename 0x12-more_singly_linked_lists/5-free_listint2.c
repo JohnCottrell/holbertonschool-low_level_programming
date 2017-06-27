fcdc826467b5eba3eea1a4f3ae9279a4beb5198b
JohnCottrell <155@holbertonschool.com>
2017-06-27 19:42:23 +0000

    add edge case if head or head pointer are null, and remove last unecessary line that set head pointer to null.

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = temp->next;
 		free(temp);
 	}
-	*head = NULL;
 }

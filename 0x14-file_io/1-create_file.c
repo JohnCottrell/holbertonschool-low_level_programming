9604f03c561c4c8772800cec305889a5dfa03726
JohnCottrell <johnpcottrell@gmail.com>
2017-07-04 23:38:12 +0000

    remove 0600 perms.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
 		if (fd == -1)
 			return (-1);
 

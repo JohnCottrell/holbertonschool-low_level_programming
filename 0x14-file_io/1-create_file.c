aed8f92b20157861bd7c1ec90c6fc15bcb44c24c
JohnCottrell <johnpcottrell@gmail.com>
2017-07-04 23:54:55 +0000

    add seperate open function in case create fails or file already exists, this also makes it possible to avoid setting perms.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,9 +13,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
-		if (fd == -1)
-			return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR, 0600);
+	if (fd == -1)
+		fd = open(filename, O_WRONLY | O_TRUNC);
+	if (fd == -1)
+		return (-1);
 
 	for (len = 0; text_content[len]; len++)
 		;

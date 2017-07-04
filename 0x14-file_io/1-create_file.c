d60a398b928a851daefbe02871213acf65e897a3
JohnCottrell <johnpcottrell@gmail.com>
2017-07-04 20:06:53 +0000

    add limits.h to header file.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR, 0600)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR, 0600);
 		if (fd == -1)
 			return (-1);
 
 	for (len = 0; text_content[len]; len++)
 		;
-	write(fd, text_content, 1);
+	write(fd, text_content, len);
 
 	return (1);
 }

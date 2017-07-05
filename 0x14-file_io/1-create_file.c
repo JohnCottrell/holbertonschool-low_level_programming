b4f2ffcbcea66d1b2483d8dfac2d1e398794a4a0
JohnCottrell <johnpcottrell@gmail.com>
2017-07-05 00:07:46 +0000

    remove user perms from second open statement.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR, 0600);
 	if (fd == -1)
-		fd = open(filename, O_WRONLY | O_TRUNC | S_IRUSR);
+		fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 

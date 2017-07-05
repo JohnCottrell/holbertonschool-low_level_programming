e4dc4d602105fdca3ac489758dcf64fb0f3384f3
JohnCottrell <johnpcottrell@gmail.com>
2017-07-05 00:15:37 +0000

    check filename for NULL at the beginning, rather than later on.

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -11,9 +11,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	int fd, len;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 
-	if (fd == -1 || filename == NULL)
+	if (fd == -1)
 		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;

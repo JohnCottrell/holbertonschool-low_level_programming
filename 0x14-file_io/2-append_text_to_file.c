7c7ca9eb1db97c005f54fc0721d7869bf97ee1bd
JohnCottrell <155@holbertonschool.com>
2017-07-06 18:05:42 +0000

    add check for NULL for text_content. Add close for fd upon premature return.

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -8,21 +8,28 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
 
-	int fd, len;
+	ssize_t len, bytes_written;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 
 	if (fd == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
-		;
-	fd = write(fd, text_content, len);
-
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 	close(fd);
-
 	return (1);
 }

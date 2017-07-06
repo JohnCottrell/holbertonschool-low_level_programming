e14bad114ea39ed961bf0e30cd0fe2850c1a42c6
JohnCottrell <155@holbertonschool.com>
2017-07-06 17:50:32 +0000

    add check for text_content as NULL, close fd.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,11 +19,17 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	for (len = 0; text_content[len]; len++)
-		;
-	len2 = write(fd, text_content, len);
-	if (len2 != len)
-		return (-1);
-
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		len2 = write(fd, text_content, len);
+		if (len2 != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
 	return (1);
 }

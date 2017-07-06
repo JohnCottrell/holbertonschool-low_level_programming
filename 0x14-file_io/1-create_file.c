2d0ed61f3cb2758a0561cded7ab47e07db94dace
JohnCottrell <155@holbertonschool.com>
2017-07-06 15:53:17 +0000

    remove 0600 perms, and add O_TRUNC to respond to parameters.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,9 +13,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR, 0600);
-	if (fd == -1)
-		fd = open(filename, O_WRONLY | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fd == -1)
 		return (-1);
 

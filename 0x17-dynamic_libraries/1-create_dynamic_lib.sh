42645454dd505d11033a4762b7323c6cab38e74d
JohnCottrell <johnpcottrell@gmail.com>
2017-08-07 22:52:48 +0000

    move *.o after shared.

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -c -Wall -Werror -Wextra -pedantic -fPIC *.c
-gcc -Wall -Werror -Wextra -pedantic -shared -o liball.so *.o
+gcc -Wall -Werror -Wextra -pedantic -shared *.o -o liball.so
